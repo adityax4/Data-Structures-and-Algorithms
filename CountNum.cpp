38b160e97b0afde6cda826bd27241c1118a59b6c
adityax4 <advensicaddy@gmail.com>
2023-12-23 12:26:19 +0530

    updated

---
+++ CountNum.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int func(int n){
+    int x=n;
+    int count=0;
+    while (x!=0)
+    {
+        x=x/10;
+        count++;
+    }
+    return count;
+}
+
+int main(){
+    int n, digits;
+    cin>>n;
+    digits=func(n);
+    cout<<"no. of digits are: "<<digits<<endl;
+    return 0;
+}
